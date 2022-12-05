5d96e27a8f18aeb95d06b9ded63224c2a50bd875
Miguel Saragoça <mig.espiritosanto@gmail.com>
2022-12-05 14:15:36 +0000

    compilador

--- compile.js
+++ compile.js
@@ -1,1 +1,12 @@
 // compile code will go here
+
+const path = require('path'); // constroi um path do compile.js para o Inbox.sol
+const fs = require('fs');
+const solc = require('solc');
+
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol'); // gera um path para o Inbox.sol na pasta contracts
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+solc.compile(source,1);
+
